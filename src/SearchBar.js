9b47051d30ffb5c5dda3edf2c8505b02bbd2384c
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-19 09:24:28 +0100

    added SearchBar

---
+++ src/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+
+
+const SearchBar = ({ products }) => {
+const [searchTerm, setSearchTerm] = useState('');
+      
+const handleSearch = (event) => {
+          setSearchTerm(event.target.value);
+        };
+      
+        const filteredProducts = products.filter((product) =>
+          product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    return (
+        <div>
+            <input type="text" placeholder="search" value={searchTerm} onChange={handleSearch}/>
+    
+          <ul>
+            {filteredProducts.map((product) => (
+            <li key={product.id}>{product.name}</li>
+            ))}
+          </ul>
+        </div>
+    )
+}
+
+export default SearchBar;

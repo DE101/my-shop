e5e4c4036622b4028a3a6766771400b5918a92bc
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-25 08:01:35 +0100

    changed content

--- src/Product.js
+++ src/Product.js
@@ -1,35 +1,35 @@
 export default function Product(props) {
     return (
-        <div class="col-lg-4 col">
-          <div class="card text-black">
-            <i class="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
+        <div className="col-lg-4 col">
+          <div className="card text-black">
+            <i className="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
             <img
               src={props.data.thumbnail}
-              class="card-img-top"
+              className="card-img-top"
               alt="Apple Computer"
             />
-            <div class="card-body">
-              <div class="text-center">
-                <h5 class="card-title">{props.data.title}</h5>
-                <p class="text-muted mb-4">{props.data.description}</p>
+            <div className="card-body">
+              <div className="text-center">
+                <h5 className="card-title">{props.data.title}</h5>
+                <p className="text-muted mb-4">{props.data.description}</p>
               </div>
               <div>
-                <div class="d-flex justify-content-between">
+                <div className="d-flex justify-content-between">
                   <span>Price</span>
                   <span>{props.data.price}</span>
                 </div>
-                <div class="d-flex justify-content-between">
+                <div className="d-flex justify-content-between">
                   <span>Discount Percentage</span>
                   <span>{props.data.discountPercentage}</span>
                 </div>
-                <div class="d-flex justify-content-between">
+                <div className="d-flex justify-content-between">
                   <span>Rating</span>
                   <span>{props.data.rating}/5</span>
                 </div>
               </div>
-              <div class="d-flex justify-content-between total font-weight-bold mt-4">
-                <span>Total</span>
-                <span>{props.data.price}</span>
+              <div className="d-flex justify-content-between total font-weight-bold mt-4">
+                <span className="itemDescription">Total</span>
+                <span className="itemDescription-excerpt">{props.data.price}</span>
               </div>
             </div>
           </div>

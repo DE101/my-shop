3be905f1a08452436a6be992556e64152356bc32
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-26 13:47:45 +0100

    save changes

--- src/Product.js
+++ src/Product.js
@@ -3,28 +3,38 @@ export default function Product(props) {
     <div className="site-container">
       <div className="itemContainer">
         <i className="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
-        <img src={props.data.thumbnail} className="img" alt="Apple Computer" />
-        <div className="text-container">
-          <h5 className="itemDescription">{props.data.title}</h5>
-          <p className="itemDescriptionExcerse">{props.data.description}</p>
-        </div>
-        <div className="text-container">
-          <span className="itemDescription">Price:</span>
-          <span className="itemDescriptionExcerse">{props.data.price}</span>
-        </div>
-        <div className="text-container">
-          <span className="itemDescription">Discount Percentage:</span>
-          <span className="itemDescriptionExcerse">
-            {props.data.discountPercentage}
-          </span>
-        </div>
-        <div className="text-container">
-          <span className="itemDescription">Rating:</span>
-          <span className="itemDescriptionExcerse">{props.data.rating}/5</span>
-        </div>
-        <div className="text-container">
-          <span className="itemDescription">Total:</span>
-          <span className="itemDescriptionExcerse">{props.data.price}</span>
+        <div className="itemCard">
+          <div className="image-container">
+            <img
+              src={props.data.thumbnail}
+              className="img"
+              alt="Apple Computer"
+            />
+          </div>
+          <div className="text-container">
+            <span className="itemDescription">{props.data.title} :</span>
+            <span className="itemDescriptionExcerse">{props.data.description}</span>
+          </div>
+          <div className="text-container">
+            <span className="itemDescription">Price:</span>
+            <span className="itemDescriptionExcerse">{props.data.price}</span>
+          </div>
+          <div className="text-container">
+            <span className="itemDescription">Discount Percentage:</span>
+            <span className="itemDescriptionExcerse">
+              {props.data.discountPercentage}
+            </span>
+          </div>
+          <div className="text-container">
+            <span className="itemDescription">Rating:</span>
+            <span className="itemDescriptionExcerse">
+              {props.data.rating}/5
+            </span>
+          </div>
+          <div className="text-container">
+            <span className="itemDescription">Total:</span>
+            <span className="itemDescriptionExcerse">{props.data.price}</span>
+          </div>
         </div>
       </div>
     </div>
